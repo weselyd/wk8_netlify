c0d851faef50d7c01920fcd41f31b3546273cf21
Dustin Wesely <weselyd@csp.edu>
2025-06-22 14:45:53 -0500

    Remove error throw for city not found in geocode function to handle empty responses later, more gracefully

--- netlify/functions/geocode.js
+++ netlify/functions/geocode.js
@@ -9,7 +9,7 @@ const owDirectGeocode = async (city) => {
   );
   if (!response.ok) throw new Error('Location lookup failed');
   const data = await response.json();
-  if (!data.length) throw new Error('City not found');
+  //if (!data.length) throw new Error('City not found');
   return data;
 };
 
